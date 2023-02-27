df7885c491f21674de8797691c8f3c10f523d35a
Duane Grell <duanegrell@gmail.com>
2023-02-27 12:34:38 -0500

    passing tests

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,70 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    spicy_food_names = [food["name"] for food in spicy_foods]
+    print (spicy_food_names)
+    return (spicy_food_names)
+
+# get_names(spicy_foods)
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    spicy_food_dictionary = [food for food in spicy_foods if food["heat_level"] > 5]
+    print (spicy_food_dictionary)
+    return (spicy_food_dictionary)
+
+# get_spiciest_foods(spicy_foods)
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for i in range(len(spicy_foods)):
+        name = spicy_foods[i]["name"]
+        cuisine = spicy_foods[i]["cuisine"]
+        heat_level = spicy_foods[i]["heat_level"]
+        peppers = "🌶" * heat_level
+        print (f'{name} ({cuisine}) | Heat Level: {peppers}')
+
+# print_spicy_foods(spicy_foods)
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+
+    for i in range(len(spicy_foods)):
+        if spicy_foods[i]["cuisine"] == cuisine:
+            print (spicy_foods[i])
+            return (spicy_foods[i])
+
+# get_spicy_food_by_cuisine(spicy_foods, "Thai")
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    for i in range(len(spicy_foods)):
+        name = spicy_foods[i]["name"]
+        cuisine = spicy_foods[i]["cuisine"]
+        heat_level = spicy_foods[i]["heat_level"]
+        peppers = "🌶" * heat_level
+
+        if heat_level > 5:
+            print (f'{name} ({cuisine}) | Heat Level: {peppers}')
+
+# print_spiciest_foods(spicy_foods)
 
 def get_average_heat_level(spicy_foods):
-    pass
+
+    heat_list = [food["heat_level"] for food in spicy_foods]
+    avg_heat = int (sum(heat_list) / len(heat_list))
+    print (avg_heat)
+    return (avg_heat)
+
+# get_average_heat_level(spicy_foods)
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+
+    spicy_foods.append(spicy_food)
+    print (spicy_foods)
+    return (spicy_foods)
+
+create_spicy_food(
+    spicy_foods,
+    {
+        'name': 'Griot',
+        'cuisine': 'Haitian',
+        'heat_level': 10,
+    }
+)
